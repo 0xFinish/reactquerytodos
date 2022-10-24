74cc27b118bdbeec7bdef8e5bbc1046372fc6567
fi9ish <tima.finish@gmail.com>
2022-10-24 11:15:16 +0200

    main

--- requests/requests.js
+++ requests/requests.js
@@ -1,19 +1,21 @@
 import axios from "axios";
+import Link from "next/link";
+
 
 export async function getTodosRequest() {
-    const response = await axios.get("http://localhost:3000/api")
+    const response = await axios.get("/api")
     return response.data
 }
 
 export async function postTodoRequest(todo) {
-    return await axios.post("http://localhost:3000/api", todo)
+    return await axios.post("/api", todo)
 }
 
 export async function deleteTodoRequest(id) {
     console.log(id)
-    return await axios.delete("http://localhost:3000/api", { data: id})
+    return await axios.delete("/api", { data: id})
 }
 
 export async function updateTodoRequest(id) {
-    return await axios.patch("http://localhost:3000/api", {data: id})
+    return await axios.patch("/api", {data: id})
 }
